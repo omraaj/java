cecf690a64c8b855276285fb6de7d2756b5ca65b
omraaj <87692987+omraaj@users.noreply.github.com>
2021-12-28 13:39:28 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{ 
+public static void main( String args[])
+{ int a = 3, b = 5, c = 7;
+  System.out.println(a+++b--+c++);
+System.out.println(--a+b+++a++);
+System.out.println(--c+a++-b++);
+System.out.println(a+b+c);
+System.out.println(a-b-c);
+}
+}
