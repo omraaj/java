9a5d76a0934d9bd378ecc3d36e9b7dd3304c032a
Omraj Nichal <87692987+omraaj@users.noreply.github.com>
2022-01-27 23:39:02 +0530

    Add files via upload

---
+++ testdisp.java
@@ -0,0 +1,16 @@
+import java .util.*;
+import pattern.*;
+class testdisp
+{
+public static void main(String args[])
+{
+{Scanner sc = new Scanner(System.in);
+System.out.println("Enter total line=");
+int line = sc.nextInt();
+System.out.println("Enter id =");
+int id = sc.nextInt();
+disp d1 = new disp();
+d1.show(line,id);
+}
+}
+}
