26e3054bc1cbc489cbe34b8fc62e20aa4c286679
Omraj Nichal <87692987+omraaj@users.noreply.github.com>
2023-01-10 09:37:15 +0530

    Add files via upload

---
+++ memory.java
@@ -0,0 +1,13 @@
+class memory 
+{
+   public static void main ( String args[])
+{ 
+   System.out.println (Byte.SIZE);
+    System.out.println (Short.SIZE/8);
+    System.out.println (Integer.SIZE/8);
+System.out.println (Long.SIZE/8);
+System.out.println (Float.SIZE/8);
+System.out.println (Double.SIZE/8);
+System.out.println (Character.SIZE/8);
+  }
+ }
